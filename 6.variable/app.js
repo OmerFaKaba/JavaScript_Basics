663146ad2bfa863af74a8cdd3cca7d51a16f3c85
Ömer Faruk Kaba <farukkaba4807@gmail.com>
2025-04-28 19:21:36 +0300

    second commit

---
+++ 6.variable/app.js
@@ -0,0 +1,91 @@
+/*
+? variable types
+
+1-String
+2-Number
+3-Boolen
+4-Null
+5-Undefined
+6-Object
+7-Function
+
+
+*/
+
+let name = "Ömer";
+
+console.log(name);
+console.log(typeof name);
+
+
+let a = "5";
+let b = "2";
+
+console.log(a+b);
+
+
+let text ="Your age : ";
+let age = 23;
+
+console.log(text+age);
+
+
+
+//boolen
+console.log(5>8);
+
+//null
+let x = null;
+console.log(x);
+
+//undefined
+
+let y ;
+console.log(y);
+console.log(typeof y);
+
+
+
+//object
+
+let person= {
+    name :"Ömer",
+    surname :"Kaba",
+    age : 23
+}
+console.log(typeof person);
+
+
+
+
+
+let numbers = [1,2,3,4,5];
+console.log(typeof numbers);
+
+
+//function
+
+let func  = function(){
+    console.log("Hello");
+}
+
+func();
+console.log(typeof func);
+
+// == 
+console.log(3=="3"); // output is true 
+
+
+
+// ===
+console.log(5==="5"); // output is false
+
+
+
+// logic operators 
+
+/*
+   && : and
+   || : or
+   !  : not
+*/
---
+++ 7.dialog boxes/app.js
@@ -0,0 +1,29 @@
+/*
+
+    - Alert
+    - Promt
+    - Confirm
+    * they are functions
+*/
+
+// console.log(window)
+
+
+
+//alert("Welcome");
+
+
+//prompt(); takes input
+/*
+let name = prompt(" Please enter your name : ");
+console.log(name);
+
+let age = prompt("please enter yourn age : ");
+console.log("Your age is " + age);
+*/
+
+
+//confirm();
+
+let result = confirm("Do you wanna delete?");
+console.log(result);
